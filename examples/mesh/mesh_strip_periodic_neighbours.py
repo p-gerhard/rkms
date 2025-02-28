d064b315ddb3a801de13cc37db49bff18e2e8171
Pierre Gerhard <p.gerhard@dunistra.fr>
2025-02-28 17:44:57 +0100

    Fix mesh example

--- examples/mesh/mesh_strip_periodic_neighbours.py
+++ examples/mesh/mesh_strip_periodic_neighbours.py
@@ -24,20 +24,6 @@ if __name__ == "__main__":
         use_periodic_bd=True,
     )
 
-    nb_neighors = 3
-    dx_offset = 0.5 * mesh.dx
-    dy_offset = 0.5 * mesh.dy
-
-    if dim == 3:
-        nb_neighors = 6
-        dz_offset = 0.5 * mesh.dz
-
-    nbs = mesh.elem2elem.reshape(-1, nb_neighors)
-    print(nbs)
-
-
-    import numpy as np
-
     nb_neighbors = 6 if dim == 3 else 3
 
     dx_offset = 0.5 * mesh.dx
@@ -66,72 +52,5 @@ if __name__ == "__main__":
         # Cells on +Z face
         nbs[mesh.cells_center[:, 2] == mesh.zmax - dz_offset, 4] = -1 
 
-    print(nbs)
-    # Flatten back
     mesh.elem2elem = nbs.flatten()
-    print(mesh.elem2elem)
-
-    exit()
-
-    # UNVECTORIZED VERSION DEPRECATED
-
-    # for idx in range(nbs.shape[0]):
-
-        # Check if the cell lies on the -X boundary face
-        # if mesh.cells_center[idx][0] == mesh.xmin + dx_offset:
-            # print("The cell lies on the boundary face with normal in the -X direction.")
-            # For a non-periodic mesh, the Left face (index 1) should have no neighbor (-1).
-            # assert nbs[idx][1] == -1
-
-            # For a periodic mesh we strip the neighbor
-            # nbs[idx][1] == -1
-
-        # Check if the cell lies on the +X boundary face
-        # if mesh.cells_center[idx][0] == mesh.xmax - dx_offset:
-            # print("The cell lies on the boundary face with normal in the +X direction.")
-            # For a non-periodic mesh, the Right face (index 0) should have no neighbor (-1).
-            # assert nbs[idx][0] == -1
-
-            # For a periodic mesh we strip the neighbor
-            # nbs[idx][0] == -1
-
-        # Check if the cell lies on the -Y boundary face
-        # if mesh.cells_center[idx][1] == mesh.ymin + dy_offset:
-            # print("The cell lies on the boundary face with normal in the -Y direction.")
-            # For a non-periodic mesh, the Back face (index 3) should have no neighbor (-1).
-            # assert nbs[idx][3] == -1
-
-            # For a periodic mesh we strip the neighbor
-            # nbs[idx][3] = -1
-
-        # Check if the cell lies on the +Y boundary face
-        # if mesh.cells_center[idx][1] == mesh.ymax - dy_offset:
-            # print("The cell lies on the boundary face with normal in the +Y direction.")
-            # For a non-periodic mesh, the Front face (index 2) should have no neighbor (-1).
-            # assert nbs[idx][2] == -1
-
-            # For a periodic mesh we strip the neighbor
-            # nbs[idx][2] = -1
-
-        # Check if the cell lies on the -Z boundary face
-        # if dim == 3 and (mesh.cells_center[idx][2] == mesh.zmin + dz_offset):
-            # print("The cell lies on the boundary face with normal in the -Z direction.")
-            # For a non-periodic mesh, the South face (index 5) should have no neighbor (-1).
-            # assert nbs[idx][5] == -1
-
-            # For a periodic mesh we strip the neighbor
-            # nbs[idx][5] = -1
-
-        # Check if the cell lies on the +Z boundary face
-        # if dim == 3 and (mesh.cells_center[idx][2] == mesh.zmax + dz_offset):
-            # print("The cell lies on the boundary face with normal in the +Z direction.")
-            # For a non-periodic mesh, the North face (index 4) should have no neighbor (-1).
-            # assert nbs[idx][4] == -1
-
-            # For a periodic mesh we strip the neighbor
-            # nbs[idx][4] = -1
-
-    # print(nbs)
-    # Flatten back
-    # mesh.elem2elem = nbs.flatten()
-    # print(mesh.elem2elem)
+    
